e7656a1e67fca459a28201cb16db0c307a33b902
2rkmen <maotsedun@mail.ru>
2021-10-27 21:00:29 +0300

    готово 1 задание, натянут шаблон

--- php720.com/task1.php
+++ php720.com/task1.php
@@ -1,35 +1,37 @@
-<!doctype html>
-<html lang="ru">
-<head>
-    <meta charset="utf-8" />
-    <title></title>
-    <link rel="stylesheet" href="style.css" />
-</head>
-<body>
-
-<header>Найти сумму цифр числа</header>
-https://php720.com/task/1
-
-
-
-Вам нужно разработать программу, которая считала бы сумму цифр числа введенного пользователем.
-Например: есть число 123, то программа должна вычислить сумму цифр 1, 2, 3, т. е. 6.
-По желанию можете сделать проверку на корректность введения данных пользователем.
-<nav>Меню навигации</nav>
-
-<aside>Боковая колонка SideBar</aside>
-
-<article>
-    Контент - основное содержимое страницы.
-</article>
 <?php
-$number = 5134;
-echo implode($number);
+$content['pageTitle'] = 'Найти сумму цифр числа';
+$content['postTitle'] = 'Найти сумму цифр числа';
+$content['date'] = '2021.10.27';
+$content['author'] = '2rkmen';
+$content['text'] =  <<<END
+<b>https://php720.com/task/1</b>
+<p>Вам нужно разработать программу, которая считала бы сумму цифр числа введенного пользователем.
+Например: есть число 123, то программа должна вычислить сумму цифр 1, 2, 3, т. е. 6.</p>
+<p>По желанию можете сделать проверку на корректность введения данных пользователем.</p>
+END;;
 
-?>
-<footer>Подвал сайта</footer>
+include('../template/head.php');
 
-</body>
-</html>
+if(empty($_POST['number'])){
+    echo <<<END
+<form action="" method="post">
+<input type="text" name="number" required class="form-control">
+<input type="submit" value="отправить">
+<input type="submit" name="reset" value="reset">
+</form>
+END;
 
+}
 
+$number = $_POST['number'];
+//echo $number;
+$arNumbers = str_split($number, 1);
+$result = 0;
+
+foreach ($arNumbers as $arNumber) {
+    $result += $arNumber;
+}
+
+echo '<p>' . $result . '</p>';
+
+include('../template/footer.php');
